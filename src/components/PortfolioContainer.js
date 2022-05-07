350d870876872c5b0260a91dc2e4c01a6565bc4f
hazel-jimenez <95867111+hazel-jimenez@users.noreply.github.com>
2022-05-07 12:57:17 -0400

    make about me default page

--- src/components/PortfolioContainer.js
+++ src/components/PortfolioContainer.js
@@ -6,7 +6,7 @@ import Resume from './pages/Resume';
 import Contact from './pages/Contact';
 
 export default function PortfolioContainer() {
-  const [currentPage, setCurrentPage] = useState('AboutMe');
+  const [currentPage, setCurrentPage] = useState('About Me');
 
   // TODO: Add a comment describing the functionality of this method
   const renderPage = () => {
